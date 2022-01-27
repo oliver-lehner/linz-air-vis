1140b6c6568427375075023b92f48f81096fd2e1
Oliver Lehner <oliver.r.lehner@gmail.com>
2022-01-27 12:17:55 +0000

    init cubed, gltf loading

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import * as SC from 'svelte-cubed';
+	import Parrot from '$lib/parrot.svelte'
+</script>
+
+<SC.Canvas>
+	<Parrot/>
+	<SC.PerspectiveCamera position={[1, 1, 3]} />
+	<SC.OrbitControls enableZoom={false} />
+	<SC.AmbientLight intensity={0.6} />
+	<SC.DirectionalLight intensity={0.6} position={[-2, 3, 2]} />
+</SC.Canvas>
