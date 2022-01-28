2e5e081fa9ec6e5b0fc8795808ac1387aaa2a2a7
Oliver Lehner <oliver.r.lehner@gmail.com>
2022-01-28 13:00:57 +0000

    rm'd code unrelated to particles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,11 @@
 <script>
 	import * as THREE from 'three'
 	import * as SC from 'svelte-cubed';
-	import Parrot from '$lib/parrot.svelte'
 	import Particles from '$lib/particles/index.svelte'
 </script>
 
 <SC.Canvas antialias background={new THREE.Color('palevioletred')}>
-	<Particles></Particles>
+	<Particles/>
 	<SC.PerspectiveCamera position={[1, 1, 1]} />
 	<SC.OrbitControls enableZoom={true} />
 	<SC.AmbientLight intensity={0.6} />
